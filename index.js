60c53d585a346e01a671c26db976f6cbc0c5d227
Pessi Päivärinne <pessip@hotmail.fi>
2020-05-08 20:28:12 +0300

    Added herokuapp support

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var prefix = config.prefix
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
-client.login(config.token)
+client.login(process.env.TOKEN);
 
 client.on('message', msg => {
     //validate message
